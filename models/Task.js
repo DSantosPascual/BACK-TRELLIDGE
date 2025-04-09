b9c8a91246ca016fd9f941dfdce166907d14bc87
DSantosPascual <d.santos.pascual@gmail.com>
2025-04-09 11:28:05 +0200

    Catergory

--- models/Task.js
+++ models/Task.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    title: String,
-    completed: Boolean
-}, { timestamps: true });
+    title: { type: String, required: true },
+    description: String,
+    completed: { type: Boolean, default: false },
+    checklist: [
+      {
+        text: String,
+        done: { type: Boolean, default: false }
+      }
+    ],
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' }
+  }, { timestamps: true });
 
 const Task = mongoose.model('Task', TaskSchema);
 
