c9c475d252db9cb2c3b0f9b54127c515da44a40e
MajinDohko <agarridoluengo@gmail.com>
2025-04-15 16:13:30 +0200

    nuevo cambio

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const cors = require('cors');
 
 app.use(cors({
-    origin: 'https://dsantospascual.github.io'
+    origin: ['https://dsantospascual.github.io', 'http://localhost:5173']
   }))
   
 
